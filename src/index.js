5b5df0cffe8af64c90b69b3a5a60d9fc8f8d3ce8
Dzmitry <nonickihave@gmail.com>
2019-02-28 00:17:05 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -2,4 +2,50 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+	var result = {};
+	if (amount > 10000) {
+		result['error'] = "You are rich, my friend! We don't have so much coins for exchange";
+
+	} else if (amount > 0) {
+		var H = 50,
+			h = 0,
+			Q = 25,
+			q = 0,
+			D = 10,
+			d = 0,
+			N = 5,
+			n = 0,
+			P = 1,
+			p = 0;
+		var am = amount*100,
+			rest;
+		h = Math.floor(amount / H);
+		rest = amount - h*H;
+		q = Math.floor(rest / Q);
+		rest = rest - q * Q;
+		d = Math.floor(rest / D);
+		rest = rest - d * D;
+		n = Math.floor(rest / N);
+		rest = rest - n * N;
+		p = rest;
+
+
+		if (h!= 0) {
+			result['H'] = h;
+		}
+		if (q != 0) {
+			result['Q'] = q;
+		}
+		if (d != 0) {
+			result['D'] = d;
+		}
+		if (n != 0) {
+			result ['N'] = n;
+		}
+		if (p != 0) {
+			result ['P'] = p;
+		}
+	}
+
+	return result;
 }
